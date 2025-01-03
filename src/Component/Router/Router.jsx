e079f569cc2bfc1fedff2c536741159605096880
Akash <shoaibsarkarakash019@gmail.com>
2025-01-03 11:23:31 +0600

    download Resume

--- src/Component/Router/Router.jsx
+++ src/Component/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Routes/Home/Home";
 import ErrorPage from "../Routes/ErrorPage/ErrorPage";
+import AboutMe from "../Routes/AboutMe/AboutMe";
 
 const router = createBrowserRouter([
   {
@@ -8,8 +9,12 @@ const router = createBrowserRouter([
     element: <Home></Home>,
     errorElement: <ErrorPage></ErrorPage>,
     children: [
-       
+     
       ],
+  },
+  {
+    path: "/aboutMe",
+    element:<AboutMe></AboutMe> ,
   },
 ]);
 
--- src/Component/Routes/AboutMe/AboutMe.jsx
+++ src/Component/Routes/AboutMe/AboutMe.jsx
@@ -1,42 +1,79 @@
+import React, { useEffect } from "react";
 import { Avatar, Typography } from "@material-tailwind/react";
-import React from "react";
-// import './AboutMe.css';  // Link to the external CSS file
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const AboutMe = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
-    <div className="bg-blue-gray-700 lg:flex justify-between items-center ">
-      <div className=" lg:w-1/3">
+    <div className="bg-blue-gray-700 lg:flex justify-between items-center">
+      <div
+        className="lg:w-1/3"
+        data-aos="fade-right"
+      >
         <img
           className="mx-auto"
           src="https://i.ibb.co.com/W6HwbJc/Akash-removebg-preview.png"
           alt=""
         />
+        <div className="text-center p-8">
+          <a
+            href="/src/assets/Mern stack resume of Akash Sarker.pdf"
+            download="My_Resume.pdf"
+            className="relative px-6 py-5 font-bold text-black group"
+          >
+            <span
+              className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"
+            ></span>
+            <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
+            <span className="relative">Download Resume</span>
+          </a>
+        </div>
       </div>
-      <div className="text-white lg:w-1/2 ">
-        <h1 className="text-7xl font-bold  text-blue-500  lg:ml-8">ABOUT ME</h1>
-        <p className="p-10 text-lg text-blue-gray-100 font-semibold">I am the hardworking and mostly spearheaded person to secure a highly rewarding position as a Full Stack MERN(MongoDb,Express.js,React.js,Node.js) Developer , leveraging my profound expertise and passion for crafting robust and scalable web applications.
+      <div
+        className="text-white lg:w-1/2"
+        data-aos="fade-down"
+      >
+        <h1 className="text-7xl font-bold text-blue-500 lg:ml-8">ABOUT ME</h1>
+        <p className="p-10 text-lg text-blue-gray-100 font-semibold">
+          I am the hardworking and mostly spearheaded person to secure a highly
+          rewarding position as a Full Stack
+          MERN(MongoDb,Express.js,React.js,Node.js) Developer, leveraging my
+          profound expertise and passion for crafting robust and scalable web
+          applications.
         </p>
-        
-        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8 ">
-        <h1 className="text-4xl font-bold  text-blue-500">Name:</h1><span>Akash Sarker</span>
+
+        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8">
+          <h1 className="text-4xl font-bold text-blue-500">Name:</h1>
+          <span>Akash Sarker</span>
         </div>
-        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8 ">
-        <h1 className="text-4xl font-bold  text-blue-500">Date of birth:</h1><span>15 JUNE 1997</span>
+        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8">
+          <h1 className="text-4xl font-bold text-blue-500">Date of birth:</h1>
+          <span>15 JUNE 1997</span>
         </div>
-        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8 ">
-        <h1 className="text-4xl font-bold  text-blue-500">Nationality:</h1><span>Bangladeshi</span>
+        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8">
+          <h1 className="text-4xl font-bold text-blue-500">Nationality:</h1>
+          <span>Bangladeshi</span>
         </div>
-        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8 ">
-        <h1 className="text-4xl font-bold  text-blue-500">Address:</h1><span>Gazipur, Dhaka</span>
+        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8">
+          <h1 className="text-4xl font-bold text-blue-500">Address:</h1>
+          <span>Gazipur, Dhaka</span>
         </div>
-        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8 ">
-        <h1 className="text-4xl font-bold  text-blue-500">Phone:</h1><span>+8801984501095</span>
+        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8">
+          <h1 className="text-4xl font-bold text-blue-500">Phone:</h1>
+          <span>+8801984501095</span>
         </div>
-        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8 ">
-        <h1 className="text-4xl font-bold  text-blue-500">E-Mail:</h1><span> <a href="mailto:akash.sarker.coder@gmail.com">akash.sarker.coder@gmail.com</a></span>
+        <div className="flex items-center text-3xl gap-1 font-sans lg:ml-8">
+          <h1 className="text-4xl font-bold text-blue-500">E-Mail:</h1>
+          <span>
+            <a href="mailto:akash.sarker.coder@gmail.com">
+              akash.sarker.coder@gmail.com
+            </a>
+          </span>
         </div>
-         
-        
       </div>
     </div>
   );
